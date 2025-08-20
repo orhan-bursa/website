27da38d2d7ce4080906aa53f9e382c9abd303202
Orhan Bursa <bursaorhann@gmail.com>
2025-08-20 22:50:51 +0300

    work on contents

--- src/components/Info/InfoSection.tsx
+++ src/components/Info/InfoSection.tsx
@@ -1,5 +1,6 @@
 'use client'
 import cn from '@/utils/cn'
+import Image from 'next/image'
 import { useState } from 'react'
 enum InfoTab {
 	about = 'about',
@@ -11,6 +12,63 @@ const tabLabels: Record<InfoTab, string> = {
 	tech: 'tech stack',
 	projects: 'projects'
 }
+
+const TECH_STACK_ASSETS = [
+	{
+		label: 'Github',
+		path: '/github.svg'
+	},
+	{
+		label: 'Gitlab',
+		path: '/gitlab.svg'
+	},
+	{
+		label: 'Next',
+		path: '/next.svg'
+	},
+	{
+		label: 'Node',
+		path: '/node.svg'
+	},
+	{
+		label: 'Notion API',
+		path: '/notion.svg'
+	},
+	{
+		label: 'Python',
+		path: '/python.svg'
+	},
+	{
+		label: 'React',
+		path: '/react.svg'
+	},
+	{
+		label: 'Vite',
+		path: '/vite.svg'
+	},
+	{
+		label: 'Tanstack',
+		path: '/tanstack.png'
+	}
+]
+
+const PROJECTS = [
+	{
+		title: 'Hilal Visits',
+		description: 'Travel Blog',
+		stack: ['Notion API', 'Nextjs', 'Vercel']
+	},
+	{
+		title: 'KLC Works',
+		description: 'Architecture Studio Website',
+		stack: ['Nextjs', 'Vercel']
+	},
+	{
+		title: 'Link Shelf',
+		description: 'Desktop App for Link Management',
+		stack: ['Electronjs', 'React', 'Vite', 'Typescript', 'Tailwind', 'Tanstack/query']
+	}
+]
 export default function InfoSection() {
 	const [tab, setTab] = useState<InfoTab>(InfoTab.about)
 
@@ -33,9 +91,40 @@ export default function InfoSection() {
 					</div>
 				)
 			case InfoTab.tech:
-				return <div className="text-start"></div>
+				return (
+					<div className="text-start">
+						<div className="flex flex-wrap gap-2 duration-300">
+							{TECH_STACK_ASSETS.map((item, ind) => (
+								<div
+									key={ind}
+									className="border-foreground-secondary hover:border-foreground h-12 w-12 rounded-xl border bg-white/80 p-1 duration-300"
+								>
+									<Image
+										className="h-full w-full object-contain"
+										src={item.path}
+										alt={item.label}
+										width={32}
+										height={32}
+									/>
+								</div>
+							))}
+						</div>
+					</div>
+				)
 			case InfoTab.projects:
-				return <div className="text-start"></div>
+				return (
+					<div className="text-start">
+						<div className="flex flex-col gap-2">
+							{PROJECTS.map((project, ind) => (
+								<div key={ind}>
+									<p>{project.title}</p>
+									<p>{project.description}</p>
+									<p>{project.stack.join(',')}</p>
+								</div>
+							))}
+						</div>
+					</div>
+				)
 			default:
 				return null
 		}
